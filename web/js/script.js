6ffb25abd454b717ffec1bf2919fe6d26d02436a
TechnikJ <jasonrobo27@gmail.com>
2024-08-14 00:50:36 +0200

    Initial Website

---
+++ web/js/script.js
@@ -0,0 +1,7 @@
+function popupinfo(e){
+    e.parentNode.querySelector('.product-info-popup').style.visibility = 'visible';
+}
+
+function closeinfo(e){
+    e.parentNode.style.visibility='hidden';
+}
